5c6ca5ce33e7d85dcc8fc4d598a7c6dc91056475
Badr <105461334+Badr979@users.noreply.github.com>
2022-07-24 20:04:57 +1200

    Add files via upload

--- Badr's draft v1.py
+++ Badr's draft v1.py
@@ -1,6 +1,8 @@
 from tkinter import*
 from tkinter import ttk
 
+root=Tk()
+
 
 def GUI(): #The Interface of the Program
     global full_name
@@ -25,6 +27,30 @@ def GUI(): #The Interface of the Program
     quantity = Entry(main_window)
     quantity.grid(row=4, column=2, padx=7, pady=7, columnspan=2)
 
+def print_details():
+    name_count = 0
+    # Create the column headings
+    Label(root, text="Row").grid(column=0, row=9)
+    Label(root, text="Customer Name").grid(column=1, row=9)
+    Label(root, text="Item hired").grid(column=2, row=9)
+    Label(root, text="Item Quantity").grid(column=3, row=9)
+    Label(root, text="Reciept number").grid(column=4, row=9)
+    Label(root, text=name_count).grid(column=0, row=name_count+10)
+    Label(root, text=(customer_details[name_count][0])).grid(column=1, row=name_count+10)
+    Label(root, text=(customer_details[name_count][1])).grid(column=4, row=name_count+10)
+    Label(root, text=(customer_details[name_count][2])).grid(column=2, row=name_count+10)
+    Label(root, text=(customer_details[name_count][3])).grid(column=3, row=name_count+10)
+    name_count += 1
+    print(name_count)
+    print_details()
+def quit():
+    root.destroy()
+
+buttonPRNT = ttk.Button(root, text="Print details", command=print_details)
+buttonQUIT = ttk.Button(root, text="Quit", command=quit)
+buttonPRNT.grid(row=7, column=1, sticky=W+E, pady=5)
+buttonQUIT.grid(row=7, column=2, sticky=W+E, pady=5)
+
 
 
 
@@ -41,3 +67,7 @@ def main():
     main_window.mainloop()
 
 main()
+
+root.geometry("500x450")
+root.mainloop()
+
